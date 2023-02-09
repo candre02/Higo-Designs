ec8d7fd2aecc7e79bc86858379a591d659c8255a
Crystal Andre <candre02.2@gmail.com>
2023-02-08 20:19:16 -0600

    added files in pages, which are HD and services

---
+++ client/src/pages/HigoDesigns.js
@@ -0,0 +1,18 @@
+// import react
+import React from 'react';
+// import chakra-ui
+import {
+    chakra,
+    Box,
+    useColorModeValue,
+    Flex,
+    SimpleGrid,
+    Icon,
+    Image,
+  } from "@chakra-ui/react";
+  
+  export default function HigoDesigins() {
+    return (
+        
+    )
+  }
